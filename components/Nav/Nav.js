451b5fbd45605d71f47ba59e8ec441523e0419f4
Gareth Hind <garethjhind@gmail.com>
2021-07-16 15:27:16 +1000

    Updated Navbar

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,7 +1,37 @@
+import Link from "next/link";
+
 export const Nav = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <a href="/">
+          <span>Gareth Hind</span> Web Developer
+        </a>
+      </div>
+      <div className="navbar__links">
+        <ul>
+          <li>
+            <Link href="/" className="nav-links">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-links">
+              <a>Story</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-links">
+              <a>Work</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-links">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
