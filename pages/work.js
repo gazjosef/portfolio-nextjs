12551de49ec3a022ab717f53c5b66ec80cc5db74
Gareth Hind <garethjhind@gmail.com>
2021-08-27 13:11:46 +1000

    Added Grid

--- pages/work.js
+++ pages/work.js
@@ -2,6 +2,12 @@ export default function Work() {
   return (
     <section className="section-work">
       <h1>Work</h1>
+      <div className="section-work__grid">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
     </section>
   );
 }
