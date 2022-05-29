caf3fa7d395d80eed6d74f0e32656ffad7b6946a
Gareth Hind <garethjhind@gmail.com>
2022-05-29 18:34:39 +1000

    Update

--- pages/articles/Portfolio.jsx
+++ pages/articles/Portfolio.jsx
@@ -1,14 +1,9 @@
 import { useRef, useEffect } from "react";
 import Image from "next/image";
-// import fs from "fs";
-// import path from "path";
-// import matter from "gray-matter";
 
 import Post from "@/components/Post";
 
 export default function Portfolio() {
-  // { projects }
-
   return (
     <article id="portfolio" className="article-portfolio">
       <section className="article-portfolio__title">
