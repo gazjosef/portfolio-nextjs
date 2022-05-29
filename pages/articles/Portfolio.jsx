5fcf63cb77d3eada1c4425094ab47266d416ee9a
Gareth Hind <garethjhind@gmail.com>
2022-05-29 18:23:26 +1000

    Test

--- pages/articles/Portfolio.jsx
+++ pages/articles/Portfolio.jsx
@@ -1,21 +1,13 @@
 import { useRef, useEffect } from "react";
 import Image from "next/image";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-// import gsap from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// gsap.registerPlugin(ScrollTrigger);
+// import fs from "fs";
+// import path from "path";
+// import matter from "gray-matter";
+
 import Post from "@/components/Post";
 
-export default function Portfolio({ projects }) {
-  //   const homeImgRef = useRef(null);
-
-  //   useEffect(() => {
-  //     const el = homeImgRef.current;
-  //     gsap.fromTo(el, { rotation: 0 }, { rotation: 180, duration: 3 });
-  //   }, []);
-  console.log("projects", projects);
+export default function Portfolio() {
+  // { projects }
 
   return (
     <article id="portfolio" className="article-portfolio">
@@ -29,32 +21,4 @@ export default function Portfolio({ projects }) {
       </section>
     </article>
   );
-}
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("projects"));
-
-  console.log("files", files);
-
-  const projects = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join("projects", filename),
-      "utf-8"
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
-  return {
-    props: {
-      projects,
-    },
-  };
 }
