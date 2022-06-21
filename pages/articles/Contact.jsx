5fe8fba4e7923f02eb69d809043ca64221b7017a
Gareth Hind <garethjhind@gmail.com>
2022-06-22 09:10:31 +1000

    Updated

--- pages/articles/Contact.jsx
+++ pages/articles/Contact.jsx
@@ -25,43 +25,45 @@ export default function Contact() {
         />
       </section>
 
-      <section className="article-contact__details" ref={contactDetailsRef}>
-        <h1 className="heading-one">Contact</h1>
-        <div className="article-contact__details--phone">
-          <h3 className="heading-three align-items">
-            <FaPhone className="align-items__icon" /> Mobile
-          </h3>
-          <p className="paragraph">0402 830 315</p>
-        </div>
+      <section>
+        <section className="article-contact__details" ref={contactDetailsRef}>
+          <h1 className="heading-one">Contact</h1>
+          <div className="article-contact__details--phone">
+            <h3 className="heading-three align-items">
+              <FaPhone className="align-items__icon" /> Mobile
+            </h3>
+            <p className="paragraph">0402 830 315</p>
+          </div>
 
-        <div className="article-contact__details--email">
-          <h3 className="heading-three align-items">
-            <FaEnvelope className="align-items__icon" /> Email
-          </h3>
-          <p className="paragraph">garethjhind@gmail.com</p>
-        </div>
+          <div className="article-contact__details--email">
+            <h3 className="heading-three align-items">
+              <FaEnvelope className="align-items__icon" /> Email
+            </h3>
+            <p className="paragraph">garethjhind@gmail.com</p>
+          </div>
 
-        <div className="article-contact__details--github">
-          <h3 className="heading-three align-items">
-            <FaGithub className="align-items__icon" /> Github
-          </h3>
-          <p className="paragraph">
-            <a href="https://github.com/gazjosef">
-              https://github.com/gazjosef
-            </a>
-          </p>
-        </div>
+          <div className="article-contact__details--github">
+            <h3 className="heading-three align-items">
+              <FaGithub className="align-items__icon" /> Github
+            </h3>
+            <p className="paragraph">
+              <a href="https://github.com/gazjosef">
+                https://github.com/gazjosef
+              </a>
+            </p>
+          </div>
 
-        <div className="article-contact__details--linkedIn">
-          <h3 className="heading-three align-items">
-            <FaLinkedin className="align-items__icon" /> LinkedIn
-          </h3>
-          <p className="paragraph">
-            <a href="https://www.linkedin.com/in/gareth-h-a32448191/">
-              https://www.linkedin.com/in/gareth-h-a32448191/
-            </a>
-          </p>
-        </div>
+          <div className="article-contact__details--linkedIn">
+            <h3 className="heading-three align-items">
+              <FaLinkedin className="align-items__icon" /> LinkedIn
+            </h3>
+            <p className="paragraph">
+              <a href="https://www.linkedin.com/in/gareth-h-a32448191/">
+                https://www.linkedin.com/in/gareth-h-a32448191/
+              </a>
+            </p>
+          </div>
+        </section>
       </section>
     </article>
   );
