3109d10c2d1ab29ce637b2b98c6b28dbed97b2c9
Gareth Hind <garethjhind@gmail.com>
2022-12-14 22:09:15 +1100

    Updated Project

--- components/Project/Project.jsx
+++ components/Project/Project.jsx
@@ -16,55 +16,57 @@ export default function Project({ project }) {
 
   return (
     <div className="post" ref={projectRef}>
-      <div className="post__title">
-        <h3 className="heading-three">{project.frontmatter.title}</h3>
-      </div>
+      <div className="post__project">
+        <div className="post__title">
+          <h3 className="heading-three">{project.frontmatter.title}</h3>
+        </div>
 
-      <a href={`${project.frontmatter.live}`}>
-        {project.frontmatter.cover_image && (
-          <Image
-            src={project.frontmatter.cover_image}
-            alt={project.frontmatter.title}
-            height={420}
-            width={600}
-            className="post__image"
-          />
-        )}
-      </a>
-
-      <div className="post__label">
-        {categories &&
-          categories.map((category, index) => (
-            <CategoryLabel key={index}>{category}</CategoryLabel>
-          ))}
-      </div>
-
-      <div className="post__description">
-        <p>{project.frontmatter.excerpt}</p>
-      </div>
-
-      <div className="post__extra">
-        <a
-          className="post__button align-items"
-          href={`${project.frontmatter.github}`}
-        >
-          <IconContext.Provider
-            value={{ className: "post__icon align-items__icon" }}
-          >
-            <FaGithub className="align-items__icon" /> Code
-          </IconContext.Provider>
+        <a href={`${project.frontmatter.live}`}>
+          {project.frontmatter.cover_image && (
+            <Image
+              src={project.frontmatter.cover_image}
+              alt={project.frontmatter.title}
+              height={420}
+              width={600}
+              className="post__image"
+            />
+          )}
         </a>
 
-        <a
-          className="post__button align-items"
-          href={`${project.frontmatter.live}`}
-        >
-          <IconContext.Provider
-            value={{ className: "post__icon align-items__icon" }}
+        <div className="post__label">
+          {categories &&
+            categories.map((category, index) => (
+              <CategoryLabel key={index}>{category}</CategoryLabel>
+            ))}
+        </div>
+      </div>
+      <div className="post__buttons">
+        <div className="post__description">
+          <p>{project.frontmatter.excerpt}</p>
+        </div>
+        <div className="post__extra">
+          <a
+            className="post__button align-items"
+            href={`${project.frontmatter.github}`}
           >
-            <FaEye className="align-items__icon" /> Live
-          </IconContext.Provider>
-        </a>
+            <IconContext.Provider
+              value={{ className: "post__icon align-items__icon" }}
+            >
+              <FaGithub className="align-items__icon" /> GitHub
+            </IconContext.Provider>
+          </a>
+
+          <a
+            className="post__button align-items"
+            href={`${project.frontmatter.live}`}
+          >
+            <IconContext.Provider
+              value={{ className: "post__icon align-items__icon" }}
+            >
+              <FaEye className="align-items__icon" /> Live
+            </IconContext.Provider>
+          </a>
+        </div>
       </div>
     </div>
   );
