c0f09419f027a704adfc95378915b33b4213f7d9
Gareth Hind <garethjhind@gmail.com>
2024-05-02 22:24:44 +1000

    Styles: Added Badges

--- components/Snippets/Label.tsx
+++ components/Snippets/Label.tsx
@@ -27,8 +27,29 @@ const Label: React.FC<LabelProps> = ({ children }) => {
     Vue: "mediumseagreen",
     $Paid: "limegreen",
   };
+  const badgeKey: { [key: string]: string } = {
+    JavaScript: "javascript-icon",
+    Jest: "jest-icon",
+    MongoDB: "mongodb-icon",
+    NextJS: "nextjs-dark-icon",
+    React: "react-icon",
+    Redux: "redux-icon",
+    SASS: "sass-icon",
+    "Sanity.io": "sanity-icon",
+    Shopify: "shopify-icon",
+    Tailwind: "tailwindcss-icon",
+    TypeScript: "typescript-icon",
+  };
 
-  return <div className={`label label--${colorKey[children]}`}>{children}</div>;
+  return (
+    <div className={"label"}>
+      <img
+        src={`/images/label-icons/${badgeKey[children]}.png`}
+        alt={`${children}`}
+      />{" "}
+      {children}
+    </div>
+  );
 };
 
 export default Label;
