d81bad897cea5453590eef178ee8e801cff01f91
Gareth Hind <garethjhind@gmail.com>
2024-07-06 18:55:41 +1000

    Refactor: Navbar

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar: React.FC = () => {
           )}
         </IconContext.Provider>
       </div>
-      <NavbarListToggleBtn toggle={toggle} on />
+      {/* <NavbarListToggleBtn toggle={toggle} on /> */}
     </nav>
   );
 };
